8a671ace0e53a224d215c1405844c9f4dc424d37
landrzejewski <landrzejewski.poczta@gmail.com>
2023-09-14 11:13:31 +0200

    reset

---
+++ run.sh
@@ -0,0 +1,1 @@
+docker compose up postgres-server zookeeper-server kafka-server mongo-server elastic-server logstash-server keycloak-server
---
+++ shop-service/src/main/java/pl/training/payments/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package pl.training.payments;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfiguration {
+
+}
