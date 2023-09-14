a5956554c70f9bef6fa0d5c51bea38c12b7851d9
landrzejewski <landrzejewski.poczta@gmail.com>
2023-09-14 12:44:03 +0200

    reset

---
+++ payments-service/src/main/java/pl/training/payments/security/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package pl.training.payments.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakGrantedAuthorityConverter());
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(config -> {})
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(config -> config.requestMatchers(new AntPathRequestMatcher("/**")).hasRole("ADMIN"))
+                .oauth2ResourceServer(config -> config.jwt(jwtConfig -> {}))
+                .build();
+    }
+
+}
