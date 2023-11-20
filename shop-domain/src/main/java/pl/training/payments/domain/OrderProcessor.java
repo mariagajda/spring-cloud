2864e5e1146de67410bfd0deaaeeba597f49d64e
landrzejewski <landrzejewski.poczta@gmail.com>
2023-11-20 15:29:58 +0100

    update

--- shop-domain/src/main/java/pl/training/payments/domain/OrderProcessor.java
+++ shop-domain/src/main/java/pl/training/payments/domain/OrderProcessor.java
@@ -1,16 +1,24 @@
 package pl.training.payments.domain;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import pl.training.payments.ports.PaymentsService;
 import pl.training.payments.ports.ShopService;
 
 @Log
+@RequiredArgsConstructor
 public class OrderProcessor implements ShopService {
 
     private static final String DEFAULT_CURRENCY = "PLN";
 
+    private final PaymentsService paymentsService;
+
     @Override
     public void place(Order order) {
-        log.info("New order with total value: %d %s".formatted(order.getTotalValue(), DEFAULT_CURRENCY));
+        var totalValue = order.getTotalValue();
+        log.info("New order with total value: %d %s".formatted(totalValue, DEFAULT_CURRENCY));
+        var paymentStatus = paymentsService.pay(totalValue, DEFAULT_CURRENCY);
+        log.info("Order payment status: %s".formatted(paymentStatus));
     }
 
 }
