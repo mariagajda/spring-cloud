b93d5ba8165daafa8088fcf4854c80ab2e12efb6
landrzejewski <landrzejewski.poczta@gmail.com>
2023-11-21 09:58:33 +0100

    update

--- shop-domain/src/main/java/pl/training/payments/domain/OrderProcessor.java
+++ shop-domain/src/main/java/pl/training/payments/domain/OrderProcessor.java
@@ -12,10 +12,11 @@ public class OrderProcessor implements ShopService {
     private static final String DEFAULT_CURRENCY = "PLN";
 
     private final PaymentsService paymentsService;
+    private final DiscountCalculator discountCalculator;
 
     @Override
     public void place(Order order) {
-        var totalValue = order.getTotalValue();
+        var totalValue = order.getTotalValue() - discountCalculator.getValue();
         log.info("New order with total value: %d %s".formatted(totalValue, DEFAULT_CURRENCY));
         var paymentStatus = paymentsService.pay(totalValue, DEFAULT_CURRENCY);
         log.info("Order payment status: %s".formatted(paymentStatus));
