9d1bb805dae5c6f57037d77207944919de668f95
landrzejewski <landrzejewski.poczta@gmail.com>
2023-11-22 16:56:24 +0100

    update

---
+++ shop-service/src/main/java/pl/training/payments/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package pl.training.payments;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(config -> config
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(config -> config
+                        .jwt(jwtConfigurer -> {})
+                )
+                .build();
+    }
+
+}
