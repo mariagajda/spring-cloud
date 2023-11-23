edf2a67f17bdc7e5473316509d07535880077401
landrzejewski <landrzejewski.poczta@gmail.com>
2023-11-23 12:36:30 +0100

    update

--- shop-service/src/main/java/pl/training/payments/adapters/payments/FeignPaymentsService.java
+++ shop-service/src/main/java/pl/training/payments/adapters/payments/FeignPaymentsService.java
@@ -2,6 +2,7 @@
 package pl.training.payments.adapters.payments;
 
 import feign.FeignException.FeignClientException;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.context.annotation.Primary;
@@ -18,18 +19,24 @@ public class FeignPaymentsService implements PaymentsService {
 
     private final PaymentsApi paymentsApi;
 
+    @Retry(name = "payments", fallbackMethod = "payFallback")
     @Override
     public boolean pay(long amount, String currency) {
         var paymentRequestDto = new PaymentRequestDto();
         paymentRequestDto.setValue("%d %s".formatted(amount, currency));
-        try {
+       // try {
             var paymentDto = paymentsApi.pay(paymentRequestDto);
             return Optional.ofNullable(paymentDto)
                     .map(payment -> payment.status.equals("STARTED"))
                     .orElse(false);
-        } catch (FeignClientException restClientException) {
-            log.info("Payment failed: " + restClientException.getMessage());
-        }
+//        } catch (FeignClientException restClientException) {
+//            log.info("Payment failed: " + restClientException.getMessage());
+//        }
+//        return false;
+    }
+
+    public boolean payFallback(long amount, String currency, RuntimeException runtimeException) {
+        log.info("######");
         return false;
     }
 
