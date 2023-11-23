c17fd67762251722dad16902588102f890a459f4
landrzejewski <landrzejewski.poczta@gmail.com>
2023-11-23 14:57:08 +0100

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 ./mvnw clean package -DskipTests
 docker build -t configuration-server configuration-server
 docker build -t discovery-server discovery-server
+docker build -t gateway-server gateway-server
